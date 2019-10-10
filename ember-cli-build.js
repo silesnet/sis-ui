f312888591c6f5d4ede09688d9d4d96607dfc57f
silesnet <>
2019-10-10 13:00:21 +0200

    Add Tailwindcss integration

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          require('postcss-import'),
+          require('tailwindcss')('./app/tailwind/config.js'),
+        ],
+      },
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
