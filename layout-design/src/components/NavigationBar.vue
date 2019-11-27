0f2d04de74f1680af6daadd24f097360bdf6d92e
silesnet <>
2019-11-27 21:15:59 +0100

    Tune navigation header spacing

--- layout-design/src/components/NavigationBar.vue
+++ layout-design/src/components/NavigationBar.vue
@@ -1,21 +1,13 @@
 <template>
-  <div class="flex items-center h-16 bg-white border-b border-gray-200">
-    <div class="relative w-full px-6 mx-auto max-w-screen">
-      <div class="flex items-center -mx-6">
-        <div class="pl-6 pr-6">
-          <div class="flex items-center">
-            <a href="#" class="block">SIS</a>
-          </div>
-        </div>
-        <nav class="relative flex flex-grow">
-          <div class="px-4">
-            <Search :suggestions="suggestions" />
-          </div>
-          <Menu :items="menuItems" />
-        </nav>
-      </div>
-    </div>
-  </div>
+  <nav class="flex items-center w-full h-16 px-4 py-6 bg-white border-b border-gray-200">
+      <a href="#" class="block text-gray-700">
+        <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24">
+          <path d="M13 20v-5h-2v5a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-7.59l-.3.3a1 1 0 1 1-1.4-1.42l9-9a1 1 0 0 1 1.4 0l9 9a1 1 0 0 1-1.4 1.42l-.3-.3V20a2 2 0 0 1-2 2h-3a2 2 0 0 1-2-2zm5 0v-9.59l-6-6-6 6V20h3v-5c0-1.1.9-2 2-2h2a2 2 0 0 1 2 2v5h3z"/></svg>
+      </a>
+      <Search :suggestions="suggestions" class="flex-grow ml-4" />
+      <a href="#" class="block ml-4 text-gray-500">test|CZ</a>
+      <Menu :items="menuItems" class="ml-4"/>
+  </nav>
 </template>
 
 <script>
