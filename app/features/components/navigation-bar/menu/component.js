c6a60c723567e4825e0d5ab59a6480923201432a
silesnet <>
2019-12-04 21:20:31 +0100

    Imlement closing menu by Esc key

--- app/features/components/navigation-bar/menu/component.js
+++ app/features/components/navigation-bar/menu/component.js
@@ -1,6 +1,22 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class extends Component {
+  @tracked
   isOpen = false;
-  
+
+  @action
+  keyPress(event) {
+    switch(event.key) {
+      case 'Escape': 
+        event.stopPropagation();
+        event.preventDefault();
+        this.isOpen = false;
+        break;
+      case 'Enter':
+        // this.reset();
+        break;
+    }
+  }
 }
