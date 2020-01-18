0038e442247f388e45d4e715f0ecfcb52582f4d9
silesnet <>
2020-01-18 21:33:53 +0100

    Add prod authrization

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,8 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import ENV from 'sis-ui/config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
+  host = ENV.apiRootURL;
   namespace = 'api';
+  authorizer =  'authorizer:oauth2';
 }
--- app/adapters/node.js
+++ app/adapters/node.js
@@ -1,6 +1,9 @@
 
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import ENV from 'sis-ui/config/environment';
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
+  host = ENV.apiRootURL;
   namespace = 'api/networks';
+  authorizer =  'authorizer:oauth2';
 }
