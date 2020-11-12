9499a7c49459b2a12f1edcd9cbade34ade9c0ce2
silesnet <>
2020-11-12 10:44:33 +0100

    Load network nodes optinos on application start

--- app/features/application/route.js
+++ app/features/application/route.js
@@ -6,9 +6,10 @@ export default class extends Route.extend(ApplicationRouteMixin) {
   @service session;
   @service currentUser;
   @service cookies;
+  @service options;
 
   beforeModel() {
-    return this._logIn().then(this._loadCurrentUser());
+    return this._logIn().then(this.currentUser.load());
   }
 
   redirect(model, transition) {
@@ -19,14 +20,9 @@ export default class extends Route.extend(ApplicationRouteMixin) {
 
   async sessionAuthenticated() {
     const _super = this._super; // eslint-disable-line
-    await this._loadCurrentUser();
+    await this.currentUser.load();
+    await this.options.load();
     _super.call(this, ...arguments);
-  }
-
-  _loadCurrentUser() {
-    return this.currentUser.load().catch(() => {
-      this.session.invalidate();
-    });
   }
 
   _logIn() {
--- app/services/options.js
+++ app/services/options.js
@@ -1,33 +1,36 @@
 import Service from '@ember/service';
 import { resolve } from 'rsvp';
-import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 import fetch from 'fetch';
 import ENV from 'sis-ui/config/environment';
 
 export default class extends Service {
   @service session;
-  @tracked user = undefined;
+  @tracked options = {
+    network: {
+      nodes: {},
+    },
+  };
 
   load() {
     const accessToken = this.session.data.authenticated.accessToken;
     if (accessToken) {
-      return fetch(`${ENV.apiRootURL}/users/session`, {
+      return fetch(`${ENV.apiRootURL}/networks/nodes/options`, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       })
         .then((response) => {
           if (!response.ok) {
-            throw new Error('Unable to load user profile.');
+            throw new Error('Unable to load node options.');
           }
           return response.json();
         })
         .then((body) => {
-          this.user = { ...body };
-          return this.user;
-        })
-        .catch(() => this.session.invalidate());
+          this.options.network.nodes = { ...body.attributes };
+          return this.options;
+        });
     }
     return resolve(undefined);
   }
