3a68cc41873eadd7b09b6b5b425198af34c6d0e7
silesnet <>
2021-01-20 11:18:22 +0100

    Add node children list to node form

--- app/features/network/node/route.js
+++ app/features/network/node/route.js
@@ -1,15 +1,24 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import RSVP from 'rsvp';
 
 export default class NetworkNodeRoute extends Route {
   @service store;
 
   model(params) {
     if (params.name === 'create') {
-      return this.store.createRecord('node');
+      return RSVP.hash({
+        node: this.store.createRecord('node'),
+        children: [],
+      });
     }
-    return this.store.findRecord('node', params.name);
+    return this.store.findRecord('node', params.name).then((node) =>
+      RSVP.hash({
+        node: node,
+        children: this.store.query('node-item', { linkTo: node.name }),
+      }),
+    );
   }
 
   @action
